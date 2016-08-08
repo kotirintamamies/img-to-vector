9eba3d48950d01cfc0e4ec4638d7d9f7d2f53553
slaitila <sebastian.laitila@cls-engineering.fi>
2016-08-08 13:07:39 +0300

    jhjhk

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 var gm = require('gm');
 var fs = require('fs');
 var Potrace = require('potrace');
+var program = require('commander')
 
-var tr = 145;
+//configure these [red, green, blue] for your photo before running
+var colorThreshholds = [180, 180, 180]
+
 start(tr);
-function start(thresh)
+
+program
+  .arguments('<inputfile>, [outputfile]')
+  .action(function(inputfile, outputfile)
+  {
+    start(thresh)
+  }
+)
+
+function start(thresh, input, output)
 {
 
-        gm('image.jpg')
-          .whiteThreshold(thresh,thresh,thresh, 1)
-          .blackThreshold(thresh+1,thresh+1,thresh+1, 0)
+        gm(input)
+          .whiteThreshold(colorThreshholds[0],colorThreshholds[1],colorThreshholds[2], 1)
+          .blackThreshold(colorThreshholds[0]+1,colorThreshholds[1]+1,colorThreshholds[2]+1, 0)
           .write('image2.jpg', function(err){
             if(!err)
             {
@@ -17,7 +29,9 @@ function start(thresh)
               {
                 Potrace.process(function()
                 {
-                  fs.writeFile("valmis.svg", Potrace.getSVG(1), function(err){}) 
+                  fs.writeFile(output, Potrace.getSVG(1), function(err){
+                    fs.unlink('image2.jpg');
+                  })
                 })
               })
             }
